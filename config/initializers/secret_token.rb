1e20bafb30da3db0b282932cdd1a95e584dd911c
eyalkornblut <eyal987@yahoo.com>
2012-07-05 17:09:03 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvaBatya::Application.config.secret_token = '88cfdeb99152bdf30ad868c90ccb05f9734b9495b5249cfb362431e4e972e41938495790321c0331ba4a85703f370dec2de5a826f9d02ad7f110a0d506934e38'
